349f28e2cc8a6823625f9e18404bedad7144e6b2
skif-cezar <33913952+skif-cezar@users.noreply.github.com>
2018-02-21 00:26:46 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
 module.exports = function warmup(temperature) {
   // your implementation here
-  if(temperature==-20){
-    return -4;
-  }
-  if(temperature==0){
-    return 32;
-  }
-  if(temperature==15){
-    return 59;
-  }
-  if(temperature==40){
-    return 104;
-  }
+ var fahr = temperature * 1.8 + 32;
+  return fahr;
 };
